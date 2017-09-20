6eefcecf5f5eeb67a9d2aac1c05f7a65d318ca70
kaiscript <teardownk@gmail.com>
2017-09-20 23:38:37 +0800

    modify

--- src/main/java/io/kaiscript/redis/JedisClient.java
+++ src/main/java/io/kaiscript/redis/JedisClient.java
@@ -3,6 +3,10 @@ package io.kaiscript.redis;
 import org.apache.commons.lang3.StringUtils;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.exceptions.JedisException;
+import redis.clients.util.SafeEncoder;
+
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by kaiscript on 2017/9/17.
@@ -23,6 +27,39 @@ public class JedisClient {
             if (jedis != null) {
                 jedis.setex(key, expire, value);
             }
+        } catch (Exception e) {
+            throw new JedisException(e);
+        }
+    }
+
+    public String setex(String key,int seconds,String value) {
+        try (Jedis jedis = manager.getJedis()) {
+            return jedis.setex(key, seconds, value);
+        } catch (Exception e) {
+            throw new JedisException(e);
+        }
+    }
+
+    public long setnx(String key,String value) {
+        try (Jedis jedis = manager.getJedis()) {
+            return jedis.setnx(key, value);
+        } catch (Exception e) {
+            throw new JedisException(e);
+        }
+    }
+
+    public <T extends Serializable> void mset(List<T> list) {
+        byte[][] keysvalues = new byte[list.size() * 2][];
+        int i = 0;
+        for (T t : list) {
+            keysvalues[i++] = SafeEncoder.encode(t.toString());
+        }
+        //
+    }
+
+    public void mset(String... keysvalues) {
+        try (Jedis jedis = manager.getJedis()) {
+            jedis.mset(keysvalues);
         } catch (Exception e) {
             throw new JedisException(e);
         }
